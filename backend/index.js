b4614a96db288cf5c04bfe0a926a9b9933eef95f
laksh-wadhwani <laksh.wadhwani55@gmail.com>
2025-11-03 05:02:03 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -5,11 +5,7 @@ const cors = require("cors")
 const bodyParser = require("body-parser")
 
 const app = express();
-app.use(cors({
-    origin: '*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}))
+app.use(cors())
 app.use(bodyParser.json())
 app.use(express.urlencoded({extended:true}))
 app.use(express.static('uploads'))
