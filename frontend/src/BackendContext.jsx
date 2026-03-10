6f3d1498bd1e1d6f2f35c00c97e351bd60404858
Laksh Wadhwani <laksh.wadhwani55@gmail.com>
2026-03-11 02:17:30 +0500

    Fix backendURL assignment in BackendContext

--- frontend/src/BackendContext.jsx
+++ frontend/src/BackendContext.jsx
@@ -3,7 +3,7 @@ import { createContext, useContext } from "react";
 export const BackendContext = createContext();
 
 export const BackendProvider = ({ children }) => {
-  const backendURL = "https://laksh-portfolio-backend.vercel.app || http://localhost:9001"
+  const backendURL = "https://laksh-portfolio-backend.vercel.app" || "http://localhost:9001"
 
   return (
     <BackendContext.Provider value={backendURL}>
